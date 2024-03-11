7c22291db3950f434f6fb6e2e4fe1961ad047337
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2024-03-11 15:07:32 +0100

    import picture

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <div>
-      <Header />
+      <div>
+        <Header />
+      </div>
+      <div className=''>
+        <LandingPage />
+      </div>
     </div>
   );
 }
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function LandingPage() {
   return (
-    <div>LandingPage</div>
+    <div className="bg-cover bg-center bg-local overflow-hidden bg-[url('./assets/Landing.jpg')]">
+        <div className="h-screen flex flex-col justify-center items-center ">
+          <h1>Hello world !</h1>
+        </div>
+        <div className=''>
+          <h1>hello world beau gosse</h1>
+        </div>
+    </div>
   )
 }
 
